b8606675a475dcaf87b73c9446aac6ff98173199
uku <hi@uku.moe>
2025-09-27 10:50:08 +0200

    feat: migrate to kotlin buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,8 @@ pluginManagement {
     repositories {
         mavenCentral()
         maven {
-            name = 'Fabric'
-            url = 'https://maven.fabricmc.net/'
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
         }
         gradlePluginPortal()
     }
