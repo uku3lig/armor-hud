00716c38616d6c5c9efe491e081abd545982fb07
uku <hi@uku.moe>
2026-06-16 18:51:37 +0200

    chore: bump version to 0.12.0

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -5,7 +5,7 @@ object BuildConfig {
     const val UKULIB_VERSION: String = "2.1.0+26.2"
     const val BEDROCKIFY_VERSION: String = "1.11.7+mc26.1"
 
-    const val MOD_VERSION: String = "0.11.0"
+    const val MOD_VERSION: String = "0.12.0"
 
     const val MODRINTH_PROJECT_ID: String = "wF189hn9"
 
